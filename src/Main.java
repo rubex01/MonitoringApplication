231a241fdf8c8d0126e863b7e01a32c3a4ca991f
rubex01 <40766757+rubex01@users.noreply.github.com>
2021-04-18 22:08:14 +0200

    Fix #4

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,19 @@
+import Functionality.Example;
 import GUI.Frame;
+import GUI.Tabs;
 
 public class Main {
 
     public static void main(String[] args) {
-
         GUI.Frame gui = new Frame();
+        Tabs tabs = gui.getTabsBar();
+
+        Example tab1 = new Example("Example file", true);
+        Example tab2 = new Example("Not closeable", true);
+        Example tab3 = new Example("Example file23", false);
+        tabs.addTab(tab1);
+        tabs.addTab(tab2);
+        tabs.addTab(tab3);
 
     }
 
